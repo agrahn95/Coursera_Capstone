55df455631514bb6dae8e59937ee833448dec68e
agrahn95 <76733684+agrahn95@users.noreply.github.com>
2021-01-14 18:22:34 +0100

    Add files via upload

---
+++ Capstone Project Course.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Capstone Project
+
+# In[10]:
+
+
+#Importing libraries
+import pandas as pd
+import numpy  as np
+print ("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
